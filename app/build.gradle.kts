d2ac862355daa3ee2e4e0c6b08a037bcf2ffeea1
aliziaee1382 <106319478+aliziaee1382@users.noreply.github.com>
2026-09-05 17:33:47 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "ir.ali0003.musicplayer.myskla"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
